796fdcbd937d56f585eb1583455ba735499ae143
MaxSchubert <mgschubert@gmail.com>
2020-01-29 12:12:14 -0500

    deleting stuff, figure v3

--- constants.R
+++ constants.R
@@ -1,8 +1,8 @@
 #global fig constants
 fig_font_size = 10
-fig_title_font_size = 16
-figure_width=7.5 #inches
+fig_title_font_size = 12
 
+figure_width=7.5 #inches
 figure_height = 10
 
 pdf.options(encoding = 'CP1250')
